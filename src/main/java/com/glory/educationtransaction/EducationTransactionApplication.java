cc9b69421f072d44667661d9f2929cfab0eb0bd9
Administrator <1528850355@qq.com>
2019-10-17 22:58:11 +0800

    交易系统初始化

---
+++ src/main/java/com/glory/educationtransaction/EducationTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.glory.educationtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glory/educationtransaction/EducationTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory.educationtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
